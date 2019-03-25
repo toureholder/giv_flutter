18a171146eea4b17d256f52f127c5417d76ab834
Touré Holder <toureholder@gmail.com>
2019-03-25 14:16:42 -0300

    :repeat: Lock screen in portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:giv_flutter/base/app.dart';
 
-void main() {
+void main() async {
   debugPaintSizeEnabled = false;
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
   runApp(new MyApp());
 }
