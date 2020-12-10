332ac6e11f03da6668e81f643e7e3358db7e0889
Touré Holder <toureholder@gmail.com>
2020-12-09 21:47:44 -0300

    :rocket: Bump version 1.9.0+24

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,6 +1,6 @@
 class Config {
-  static const versionName = '1.8.1';
-  static const buildNumber = 23;
+  static const versionName = '1.9.0';
+  static const buildNumber = 24;
   static const clientName = 'mobile';
   static const baseUrl = 'https://api.alguemquer.com.br';
   static const defaultCountryCallingCode = '55';
